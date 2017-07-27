1a93b759db0e84fd8ec2d4e8eb5443e6bc54796b
Mannuru <arunm057@gmail.com>
2017-07-26 22:02:07 -0400

    reverse,evaluatepostfix

--- src/InfixToPostfix.java
+++ src/InfixToPostfix.java
@@ -19,50 +19,75 @@ public class InfixToPostfix {
 	/**
 	 * @param args
 	 */
-	static int operatorPre(char ch){
-	
-		switch(ch){
-		case '+' : 
-		case '-' : return 1;
-		case '*' : 
-		case '/' : return 2;
-		case '^' : return 3;
-		}
-		return -1;
-	}	
-	/*
-	 * The main method that converts given infix expression to post fix expression.
-	 * 
-	 */
-	public static String infixToPostFix(String exp){
-		
-		String result = new String("");
-		
-		Stack<Character> stack = new Stack<>();
-		
-		for(int i=0; i < exp.length(); ++i){
-			char ch = exp.charAt(i);
-			
-			if(Character.isLetterOrDigit(ch))
-				result += ch;
-			else if(ch == '(')
-					stack.push(ch);
-			else if(ch == ')'){
-				while(!stack.isEmpty() && stack.peek() != '(')
-					result += stack.pop();
-				if(!stack.isEmpty() && stack.peek() != '(')
-					return "Invalid Expression";
-				else{
-					while(!stack.isEmpty() && operatorPre(ch) <= operatorPre(stack.peek()))
-						result += stack.pop();
-					stack.push(ch);
-				}
-			}
-		}
-		while(!stack.isEmpty())
-			result += stack.pop();
-		return result;
-	}
+	// A utility function to return precedence of a given operator
+    // Higher returned value means higher precedence
+    static int Prec(char ch)
+    {
+        switch (ch)
+        {
+        case '+':
+        case '-':
+            return 1;
+      
+        case '*':
+        case '/':
+            return 2;
+      
+        case '^':
+            return 3;
+        }
+        return -1;
+    }
+      
+    // The main method that converts given infix expression
+    // to postfix expression. 
+    static String infixToPostFix(String exp)
+    {
+        // initializing empty String for result
+        String result = new String("");
+         
+        // initializing empty stack
+        Stack<Character> stack = new Stack<>();
+         
+        for (int i = 0; i<exp.length(); ++i)
+        {
+            char c = exp.charAt(i);
+             
+             // If the scanned character is an operand, add it to output.
+            if (Character.isLetterOrDigit(c))
+                result += c;
+              
+            // If the scanned character is an '(', push it to the stack.
+            else if (c == '(')
+                stack.push(c);
+             
+            //  If the scanned character is an ')', pop and output from the stack 
+            // until an '(' is encountered.
+            else if (c == ')')
+            {
+                while (!stack.isEmpty() && stack.peek() != '(')
+                    result += stack.pop();
+                 
+                if (!stack.isEmpty() && stack.peek() != '(')
+                    return "Invalid Expression"; // invalid expression                
+                else
+                    stack.pop();
+            }
+            else // an operator is encountered
+            {
+                while (!stack.isEmpty() && Prec(c) <= Prec(stack.peek()))
+                    result += stack.pop();
+                stack.push(c);
+            }
+      
+        }
+      
+        // pop all the operators from the stack
+        while (!stack.isEmpty())
+            result += stack.pop();
+      
+        return result;
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String exp = "a+b*(c^d-e)^(f+g*h)-i";
